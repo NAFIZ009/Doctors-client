5e6603b27652a9aee7622f41405541e46cb28fd0
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-01-12 02:00:00 +0600

    dashboard running

--- src/pages/Routes/Routes/Routes.js
+++ src/pages/Routes/Routes/Routes.js
@@ -5,6 +5,9 @@ import Login from "../../Login/Login";
 import DisplayError from "../../../pages/shared/DisplayError/DisplayError";
 import SignUp from "../../SignUp/SignUp";
 import Appointment from "../../Appointment/Appointment/Appointment";
+import MyAppointment from "../../Dashboard/MyAppointment/MyAppointment";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import DashboardLayout from "../../../Layout/DashboardLayout";
 
 const router = createBrowserRouter([
     {
@@ -28,6 +31,17 @@ const router = createBrowserRouter([
                 path: '/appointment',
                 element: <Appointment></Appointment>
             }
+        ]
+    },
+    {
+        path: '/dashboard',
+        element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+        errorElement: <DisplayError></DisplayError>,
+        children: [
+            {
+                path: '/dashboard',
+                element: <MyAppointment></MyAppointment>
+            },
         ]
     }
 ])
